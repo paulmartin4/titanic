0a4e977fa3d0513be9039010fd31eecc7202887d
corentinconsigny <ccy184@gmail.com>
2018-11-18 19:51:32 +0100

    J'ai changé tout le data processing, ça m'a l'air un peu mieux comme ça

--- main_perso.py
+++ main_perso.py
@@ -14,106 +14,47 @@ raw_testing_df = pd.read_csv('kaggle_data/test.csv', delimiter = ',')
 ## Feature engineering
 
 training_df = raw_training_df
+testing_df = raw_testing_df
 
-#Colonne Name :
+df = [training_df, testing_df]
 
-def modify_name(name):
+# Completer Age, Embarked, Fare
+
+for data in df:
+    # Completer Age avec la moyenne
+    data.Age.fillna(data.Age.mean(), inplace = True)
+    
+    # Completer Embarked avec mode (la feature la plus présente)
+    data.Embarked.fillna(data.Embarked.mode(), inplace = True)
+    
+    # Completer Fare avec la moyenne
+    data.Fare.fillna(data.Fare.mean(), inplace = True)
+    
+# Enlever PassegengerId, Cabin et Ticket
+
+for data in df:
+    data.drop(['PassengerId', 'Cabin', 'Ticket'], axis = 1)
+    
+# Modifications sur les colonnes
+
+for data in df:
+    # Rassemble SibSp et Parch en FamilySize
+    data['FamilySize'] = data.SibSp + data.Parch + 1
+    
+    # Creation d'une colonne IsAlone (1 si oui 0 sinon)
+    data['IsAlone'] = 1
+    data.IsAlone.loc[data.FamilySize > 1] = 0  #loc donne les indices qui verifient le booleen en argument
+    
     '''
-    Modifie le nom d'un passager
-    
-    Modifie le nom d'un passager pour le rendre exploitable en ne gardant que
-    les informations utiles (on enlève le nom en tant que tel pour ne garder 
-    que 'M.', 'Mrs.' etc...)
-    
-    Args:
-        name (string) : Le nom du passager
-        
-    Returns:
-        (string) : L'attribut du passager (M., Mr. etc...)
+    .str.split coupe le string en 2 au niveau du premier argument
+    Ici il est fait 2 fois pour recuperer le titre des gens (tous les noms sont construits de la meme facon du coup c'est facile a recuperer)
     '''
-    if 'Mr.' in name:
-        return 'Mr.'
-    elif 'Miss.' in name:
-        return 'Miss.'
-    elif 'Mrs.' in name:
-        return 'Mrs.'
-    elif 'Master.' in  name:
-        return 'Master.'
-    elif 'Rev.' in name:
-        return 'Rev.'
-    else:
-        return np.nan
+    data['Title'] = data.Name.str.split(', ', expand = True)[1].str.split(".", expand = True)[0]
+
     
-training_df.Name = training_df['Name'].apply(modify_name)
-
-# Colonne Cabin
-
-def fdeck(cabin):
     '''
-    Retire le nom du pont du nom de la cabine
-    
-    Args:
-        cabin (string) : Nom de la cabine
-        
-    Returns:
-        deck (string) : Nom du pont
+    .cut attribue des intervalles réguliers aux variables 'continues' Fare et Age, pour les potentiels decision trees, le deuxième argument donne le nombre d'intervalles voulus, j'ai pris 6 au hasard
     '''
-    if type(cabin) == str and len(cabin) < 5:
-        return cabin[0]
-    else:
-        return np.nan
+    data['FareRange'] = pd.cut(data.Fare, 6)
+    data['AgeRange'] = pd.cut(data.Age, 6)
     
-def fnroom(cabin):
-    '''
-    Retire le numéro de la cabine du nom de la cabine
-    
-    Args:
-        cabin (string) : Nom de la cabine
-        
-    Returns:
-        room (int) : Numéro de la chambre
-    '''
-    if type(cabin) == str and len(cabin) < 5:
-        return cabin[1:]
-    else:
-        return np.nan
-
-deck = pd.DataFrame(training_df.Cabin).applymap(fdeck)
-deck.columns = ['Deck']
-
-room = pd.DataFrame(training_df.Cabin).applymap(fnroom)
-room.columns = ['Room']
-
-training_df = pd.concat([training_df, deck, room], axis = 1)
-training_df = training_df.drop(['Cabin'], axis = 1)
-
-training_df = training_df.drop(['Ticket'], axis = 1)
-
-## Transforamtion colonne sexe :
-
-def modify_sex(sex):
-    '''
-    Transformation de la donnée pour la rendre interprétable
-    
-    Args:
-        sex (string) : Sexe du passager
-    Returns:
-        O ou 1 (int)
-    '''
-    if sex == 'male':
-        return 0
-    if sex == 'female':
-        return 1
-    
-training_df = training_df.dropna(axis = 1, how = 'any')
-training_df = training_df.drop(['Pclass', 'PassengerId'], axis = 1)    
-
-training_df.Sex = training_df['Sex'].apply(modify_sex)
-
-# Normalisation de la colonne "Fare"
-min = training_df.Fare.min()
-max = training_df.Fare.max()
-mean = training_df.Fare.mean()
-std = training_df.Fare.std()
-    
-training_df.Fare = (training_df.Fare - mean)/(std)
